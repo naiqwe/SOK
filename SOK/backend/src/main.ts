309a1eae23695b0d511b0092f3e82773b8fefc37
Nikita Karpov <jacquesjaws@yandex.ru>
2024-03-01 21:29:16 +0400

    Выключил Cors

--- SOK/backend/src/main.ts
+++ SOK/backend/src/main.ts
@@ -8,7 +8,7 @@ async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors();
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 
