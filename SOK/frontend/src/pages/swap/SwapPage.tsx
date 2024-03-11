1e4d0b8d1abd5aa51f4774be6ccaa52d3684aeb6
Maksim Sosnin <maxim-sosnin-2000@yandex.ru>
2024-03-11 10:15:21 +0400

    Feat (Верстка страницы обмена книгами)

---
+++ SOK/frontend/src/pages/swap/SwapPage.tsx
@@ -0,0 +1,86 @@
+import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { AppLayout } from "../../components/appLayout/AppLayout";
+import { useState } from "react";
+import { CustomTabPanel } from "./tabPanel/CustomTabPanel";
+import { OfferForm } from "./offerForm/OfferForm";
+
+export const SwapPage = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  function a11yProps(index: number) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+
+  return (
+    <AppLayout>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h1" component="h2" fontSize={44} mt={"20px"}>
+          Бланк обмена
+        </Typography>
+        <Box sx={{ width: "100%" }}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="basic tabs example"
+              variant="fullWidth"
+            >
+              <Tab
+                label="Хочу обменять"
+                sx={{
+                  ":hover": {
+                    bgcolor: "inherit",
+                  },
+                  width: "33.33333%",
+                }}
+                {...a11yProps(0)}
+              />
+              <Tab
+                label="Хочу получить"
+                sx={{
+                  ":hover": {
+                    bgcolor: "inherit",
+                  },
+                  width: "33.33333%",
+                }}
+                {...a11yProps(1)}
+              />
+              <Tab
+                label="Адрес доставки"
+                sx={{
+                  ":hover": {
+                    bgcolor: "inherit",
+                  },
+                  width: "33.33333%",
+                }}
+                {...a11yProps(2)}
+              />
+            </Tabs>
+          </Box>
+          <CustomTabPanel value={value} index={0}>
+            <OfferForm></OfferForm>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            Item Two
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            Item Three
+          </CustomTabPanel>
+        </Box>
+      </Box>
+    </AppLayout>
+  );
+};
---
+++ SOK/frontend/src/pages/swap/offerForm/OfferForm.tsx
@@ -0,0 +1,39 @@
+import { Box } from "@mui/material";
+import { BookForm } from "../bookInputs/BookInputs";
+import { CategoryForm } from "../categoryInputs/CategoryInputs";
+import { useInput } from "../../../hooks/useInput";
+
+interface IBookData {
+  authorLastName: string;
+  authorFirstName: string;
+  bookName: string;
+  isbn?: string;
+  bookYear: string;
+}
+
+export const OfferForm = () => {
+  const authorLastName = useInput("", { minLength: 8, inputType: "text" });
+  const authorFirstName = useInput("", { minLength: 8, inputType: "text" });
+  const bookName = useInput("", { minLength: 8, inputType: "text" });
+  const isbn = useInput("", { minLength: 8, inputType: "text" });
+  const bookYear = useInput("", { minLength: 4, inputType: "text" });
+
+  return (
+    <form>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        {/* 1 */}
+        <BookForm
+          authorLastName={authorLastName}
+          authorFirstName={authorFirstName}
+          bookName={bookName}
+          isbn={isbn}
+          bookYear={bookYear}
+        ></BookForm>
+        {/* 2 */}
+        <CategoryForm></CategoryForm>
+      </Box>
+
+      <button>{"Зарегестрироваться"}</button>
+    </form>
+  );
+};
